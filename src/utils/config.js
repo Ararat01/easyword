d1d33684a12a498582ec8a042b9d8c6f596458b1
Ararat01 <ararat12bars@gmail.com>
2024-03-31 17:11:58 +0400

    updated url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,2 @@
-// const API_URL = "https://w2w-backend-ararat01.onrender.com";
-const API_URL = "http://192.168.8.101:4444";
+const API_URL = "https://ew-backend-k5ac.onrender.com";
 export default API_URL;
